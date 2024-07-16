678e4af9dc314f398f131db31d7193f3a63ea0f1
Güven Değirmenci <16341937+Duoquote@users.noreply.github.com>
2024-07-16 13:30:10 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as L from 'leaflet';
 import { createLayerComponent, updateGridLayer, LeafletContextInterface, LayerProps } from '@react-leaflet/core';
-import 'leaflet.gridlayer.googlemutant/dist/Leaflet.GoogleMutant';
 import { Loader, LoaderOptions } from '@googlemaps/js-api-loader';
 
 
